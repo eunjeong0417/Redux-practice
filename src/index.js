c330581d82149f933ebe4728db3545cb10a66cdd
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-08-24 13:08:34 +0900

    code refactoring

--- src/index.js
+++ src/index.js
@@ -4,26 +4,36 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus")
 const number = document.querySelector("span")
 
+const ADD = "Add";
+const MINUS = "Minus";
+
 //reducer
 const countModifier = (count = 0, action) => {
-  if (action.type === 'Add') {
-    return count + 1;
-  } else if (action.type === 'Minus') {
-    return count - 1;
-  } else {
-    return count;
-  }
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+ }
 };
 
 const countStore = createStore(countModifier);
 
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
 
-countStore.dispatch({ type: 'Add' })
-countStore.dispatch({ type: 'Add' })
-countStore.dispatch({ type: 'Add' })
-countStore.dispatch({ type: 'Add' })
-countStore.dispatch({ type: 'Add' })
-countStore.dispatch({ type: 'Add' })
-countStore.dispatch({ type: 'Minus' })
+countStore.subscribe(onChange);
 
-console.log(countStore.getState())
+const handleAdd = () => {
+  countStore.dispatch({type: ADD})
+}
+
+const handleMinus = () => {
+  countStore.dispatch({type: MINUS})
+}
+
+add.addEventListener('click', handleAdd)
+minus.addEventListener('click', handleMinus)
